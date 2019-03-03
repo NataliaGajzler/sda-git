5d363f47e11b9c4f5ce21d9e6ca16652c405f20e
NataliaGajzler <NataliaGajzler@users.noreply.github.com>
2019-03-03 09:14:15 +0100

    Natalia

--- src/OperationValidator.java
+++ src/OperationValidator.java
@@ -1,5 +1,11 @@
 public class OperationValidator {
     public static void validate(String działanie) {
-        throw new NoSuchMethodError();
+        if (działanie.trim().length()>1 &&
+        !działanie.trim().matches("/[+,-,*/]/g"))
+         // jeżeli warunek jest spełniony rzucamy wyjątek
+        throw new UnsupportedOperationException(
+                "To nie jest znak działania matematycznego!");
     }
+
+    // ctrl + k        commit
 }
