9f3c8d0b4e9d032c1af84ffc8fde04eef86bbfd1
NataliaGajzler <NataliaGajzler@users.noreply.github.com>
2019-03-03 09:37:40 +0100

    Natalia

--- src/InputManager.java
+++ src/InputManager.java
@@ -1,9 +1,22 @@
+import javax.swing.*;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class InputManager {
+
+    private static Scanner scanner = new Scanner(System.in); // static bo metoda poniżej statyczna
+
     public static String getOperation() {
-        throw new NoSuchMethodError();
+        System.out.println("Podaj znak operacji matematycznej, którą chcesz wykonać");
+        return scanner.next();
+
+       // JOptionPane.showInputDialog("Podaj znak operacji matematycznej, którą chcesz wykonać");
     }
 
     public static String getNumber() {
-        throw new NoSuchMethodError();
+        System.out.println("Podaj liczbę dla której chcesz wykonac działanie");
+
+        return scanner.next();
+
     }
 }
