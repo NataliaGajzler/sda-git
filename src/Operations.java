40f1f69b859dc085642d576f76e38f6312993f59
NataliaGajzler <NataliaGajzler@users.noreply.github.com>
2019-03-03 09:43:46 +0100

    Natalia

--- src/Operations.java
+++ src/Operations.java
@@ -1,5 +1,21 @@
 public class Operations {
-    public static int giveAnswer() {
-        throw new NoSuchMethodError();
+    public static int giveAnswer(String znak, String liczba1, String liczba2) {
+
+        switch (znak){
+            case"+":
+               return Integer.valueOf(liczba1)+Integer.valueOf(liczba2);
+
+            case"-":
+                return Integer.valueOf(liczba1)-Integer.valueOf(liczba2);
+
+            case"*":
+                return Integer.valueOf(liczba1)*Integer.valueOf(liczba2);
+
+            case"/":
+                return Integer.valueOf(liczba1)/Integer.valueOf(liczba2);
+                default:
+                    throw new IllegalArgumentException();
+
+        }
     }
 }
